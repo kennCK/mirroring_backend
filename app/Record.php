602567579d96710f4174b179c7957df2e52a161b
kennCK <kennettecanales@gmail.com>
2018-06-12 08:07:55 +0800

    update

--- app/Record.php
+++ app/Record.php
@@ -4,7 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Record extends Model
+class Record extends APIModel
 {
-    //
+    protected $table = 'records';
+    protected $fillable = ['account_id', 'url', 'code'];
 }
